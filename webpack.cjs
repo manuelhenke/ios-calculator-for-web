5adc23c907172d4214566db6e767e95b46f4f8f9
Manuel Henke <manuel.henke@yahoo.de>
2022-02-24 13:24:08 +0100

    add autoprefixer, babel and multiple output bundles

--- webpack.cjs
+++ webpack.cjs
@@ -1,10 +1,22 @@
+const webpack = require('webpack');
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
+/** @type {webpack.Configuration} */
 module.exports = {
   mode: 'production',
   devtool: false,
   entry: {
     'ios-calculator': './src/iOSCalculator.js',
+    'ios-calculator.min': './src/iOSCalculator.js',
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyJsPlugin({
+        include: /\.min\.js$/,
+      }),
+    ],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -14,12 +26,15 @@ module.exports = {
   module: {
     rules: [
       {
-        // HTML for shadow DOM
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+      },
+      {
         test: /\.html$/i,
         loader: 'html-loader',
       },
       {
-        // CSS for shadow DOM
         test: /\.s[ac]ss$/i,
         use: ['css-loader', 'postcss-loader', 'sass-loader'],
         sideEffects: true,
